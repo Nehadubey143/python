9a3dd612a569b63515777fca92ee2a109c775571
nehadubey143 <nehadubey09908@gmail.com>
2026-09-22 22:06:33 +0530

    day.12

---
+++ day12.py
@@ -0,0 +1,67 @@
+# WHY SOSync AI Tech Traning Program ????????
+# Rohan :--                          Rohit :--
+
+# Rohan in 2nd semester              Rohit:- 
+
+# => rohan invest XYZ thousand 
+#    => shi time pe shi guidance
+#    => Industrial knowledge
+#    => Proper Mentorship
+#    => Opportunity to build so many live projects
+#    => Certificate to enhance his Resume/Profile
+#    => after completion of Training Program he can get Internship and after completion of Internship he can get full time employeement with a decent package.
+#    => he can improve his skills as well as Communication skills 
+#    => he can build so many connections .
+
+
+# Trapti and Vaishnavi => 15 - 20 Days 
+# Pankaj => NOvember
+
+
+# 1. Programming Language ?
+# => it is used create a communication between a person and computer. 
+
+# Types :-
+
+#  there are 2 types of Programming Language. 
+# 1. High Level Language :- 
+# => it is software related/oriented.
+# => it is used to build websites, softwares , AI tools , mobile application and all.
+# Examples : Python, Java, C++ , Php, Javascript etc....
+
+# 2. Low Level Language :- 
+# => it is Hardware related/oriented.
+# => it is used in Induction , Mouse, Keyboard, Joystick etc.
+# Example : Machine Language , Assembly language etc.
+
+
+# 1. High Language :-
+
+# there are 3 types of Language :-
+
+# a. Compiled Language :-
+# => it runs the entire code at a time.
+# => it takes less time to run the code.
+# Example : Java, C, C++ etc.
+
+
+# b. Interpreted Language :-
+# => it runs the code line by line.
+# => it takes more time to run the code .
+# Example : Python etc.
+
+
+
+# c. Hybrid Language :-
+# => it the combination of Interpreted and Compiled Language.
+# Example: JavaScript etc.
+
+
+# print("Ayush")
+# print("Shashant")
+# print("Aman")
+# prin("Nisha")
+# print("xyz")
+# print("SOSync AI Tech IT Solutions")
+
+
